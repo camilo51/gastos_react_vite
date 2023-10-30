902dc40805e2bccee6ef955e3e6e341f6b0d422d
Camilo Pereira <pereiracamilo51@gmail.com>
2023-10-30 15:23:07 -0500

    Primer Commit

---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,48 @@
+import Gasto from "./Gasto";
+
+const ListadoGastos = ({
+  setGastoEditar,
+  gastos,
+  eliminarGasto,
+  filtro,
+  gastosFiltrados,
+}) => {
+  return (
+    <div className="listado-gastos contenedor">
+      {filtro ? (
+        <>
+          <h2>
+            {gastosFiltrados.length
+              ? "Gastos"
+              : "No Hay Gastos en esta categoría"}
+          </h2>
+          {gastosFiltrados.map((gasto) => {
+            return (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <>
+          <h2>{gastos.length ? "Gastos" : "No Hay Gastos Aún"}</h2>
+          {gastos.map((gasto) => {
+            return (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+export default ListadoGastos;
